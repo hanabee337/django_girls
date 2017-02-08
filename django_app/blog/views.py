827fc6f84a74aa415eed4a9ce61b979f0ee87964
hanabee337 <hanabee337@gmail.com>
2017-02-09 05:21:28 +0900

    Create templates & Use templates tag
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,6 +1,12 @@
 # urls.py에서 호출되어 실행
 from django.shortcuts import render
 
+from .models import Post
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    context = {
+        # 객체를 넘겨줄 때는 '_'로 구분
+        'post_list': Post.objects.all(),
+    }
+    return render(request, 'blog/post_list.html', context)
